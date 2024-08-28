606ebe8b53c57895329243e623f0bbd8bd826263
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-08-28 14:58:19 +0000

    dm

--- item-details.js
+++ item-details.js
@@ -137,7 +137,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (message) {
                     // Send the message to the bot
                     const message_content = {
-                        type: 'message',
+                        dm: 'dm',
                         message: message,
                         receiver_chat_id: chat_id
                     };
