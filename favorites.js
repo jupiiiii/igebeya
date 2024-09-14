27976feda21a15a078fa01088a9d4dadf95d825d
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-09-14 21:42:01 +0000

    00

--- favorites.js
+++ favorites.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
     tg.onEvent('backButtonClicked', function() {
         // Go to the previous page using Telegram's built-in back button functionality
         window.history.back();  // You can use custom logic here as well
+        alert('000');
     });
 
     window.addEventListener('popstate', function(event) {
