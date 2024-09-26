ff05a09517e8384f1829f464d6c9877eba71db6f
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-09-26 17:02:30 +0000

    sfs

--- sell.js
+++ sell.js
@@ -115,7 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (selectedMainCategory) {
             selectedMainCategoryDisplay.textContent = selectedMainCategory; // Update the displayed category
             mainCategoryInput.value = selectedMainCategory; // Set the hidden input value
-            console.log(mainCategoryInput)
+            // console.log(mainCategoryInput)
             mainDropdown.classList.add("hidden"); // Hide the dropdown after selection
             subCatForm.classList.toggle("hidden");
         }
