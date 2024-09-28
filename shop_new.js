e90ff500162b012a3d4144bf3241500a5e93adad
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-09-28 19:55:16 +0000

    hg

--- shop_new.js
+++ shop_new.js
@@ -58,7 +58,7 @@ function checkAndSendExistingData() {
         // Check if the session for this chat ID exists, if not, initialize
         userSessionData[chatId] = userSessionData[chatId] || {};
         userSessionData[chatId][currentTimestamp] = {}; // Create a new entry for the current session
-        console.log(user);
+        console.log(userSessionData);
         localStorage.setItem('userSessionData', JSON.stringify(userSessionData));
     }
 }
