47bc69e635844bd02e779c40bb5088cf68fa6aae
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-09-28 14:54:39 +0000

    sdfdsfggvd

--- item-details.js
+++ item-details.js
@@ -1,9 +1,7 @@
 // Initialize the Telegram WebApp instance
 const tg = window.Telegram.WebApp;
 let userSessionData = localStorage.getItem('userSessionData');
-let currentTimestamp = Object.keys(userSessionData).pop();
-console.log("Current:", currentTimestamp);
-console.log("Session:", userSessionData);
+let currentTimestamp = Object.keys(JSON.parse(storedData);).pop();
 
 
 // Function to track user interaction with items
