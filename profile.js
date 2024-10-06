3ee5523238eec725466b4be32f724174c1f7bd46
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-06 14:17:46 +0000

    Profile section

---
+++ profile.js
@@ -0,0 +1,10 @@
+// Retrieve user data from Telegram WebApp
+const user = window.Telegram.WebApp.initDataUnsafe.user;
+const firstName = user.first_name;
+const lastName = user.last_name || '';  // Last name is optional
+
+        
+// Set user's name and profile picture
+document.getElementById('user-name').innerText = `${user.first_name} ${user.last_name || ''}`;
+const profilePicture = user.photo_url || 'bg.png'; // Fallback if no profile picture is available
+document.getElementById('profile-picture').src = profilePicture;
