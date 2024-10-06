090cf2fdf08c431b4fa23027029c12271f37e10b
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-06 15:02:28 +0000

    sd

--- profile.js
+++ profile.js
@@ -3,9 +3,9 @@ const user = window.Telegram.WebApp.initDataUnsafe.user;
 const firstName = user.first_name;
 const lastName = user.last_name || '';  // Last name is optional
 
+
         
 // Set user's name and profile picture
 document.getElementById('user-name').innerText = `${user.first_name} ${user.last_name || ''}`;
-alert(user.photo_url);
 const profilePicture = user.photo_url || 'bg.png'; // Fallback if no profile picture is available
-document.getElementById('profile-picture').src = profilePicture;
+document.getElementById('profile-picture').src = `https://api.adorable.io/avatars/256/${firstName}.png`;
