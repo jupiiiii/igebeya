e64a042d036508e06188f63ec9467988c686e30e
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-06 14:21:11 +0000

    pp

--- profile.js
+++ profile.js
@@ -6,5 +6,6 @@ const lastName = user.last_name || '';  // Last name is optional
         
 // Set user's name and profile picture
 document.getElementById('user-name').innerText = `${user.first_name} ${user.last_name || ''}`;
+alert(user.photo_url);
 const profilePicture = user.photo_url || 'bg.png'; // Fallback if no profile picture is available
 document.getElementById('profile-picture').src = profilePicture;
