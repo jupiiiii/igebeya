3e4cff3677f3a7668238dc78cb70306958155d4a
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-07 18:02:02 +0000

    ds

--- profile.js
+++ profile.js
@@ -17,8 +17,32 @@ function generateRandomAlphabet(length) {
 
 // Example usage: Generate a random string of 5 characters
 const randomString = generateRandomAlphabet(5);
-        
-// Set user's name and profile picture
-document.getElementById('user-name').innerText = `${user.first_name} ${user.last_name || ''}`;
-const profilePicture = user.photo_url || 'bg.png'; // Fallback if no profile picture is available
-document.getElementById('profile-picture').src = `https://api.dicebear.com/9.x/bottts/svg?seed=${randomString}`;
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    // Set user's name and profile picture
+    document.getElementById('user-name').innerText = `${user.first_name} ${user.last_name || ''}`;
+    const profilePicture = user.photo_url || 'bg.png'; // Fallback if no profile picture is available
+    document.getElementById('profile-picture').src = `https://api.dicebear.com/9.x/bottts/svg?seed=${randomString}`;
+
+    // API fetch profile details
+    function profile_details(){
+        fetch(`https://igebeya3-272f297966dc.herokuapp.com/user_status?chat_id=${chatId}`)
+            .then(response => response.json())
+            .then(status => {
+                // alert(status.info);
+
+                // Check the user status and forward accordingly
+                if (status.info === "Logged_in") {
+                    window.location.href = "/shop.html";
+                } else if (status.info === "Logged_out") {
+                    window.location.href = "/login.html";
+                } else if (status.info === "New_user"){
+                    window.location.href = "/signup.html";
+                }
+            })
+            .catch((error) => console.error('Error:', error));
+    }
+
+
+});
