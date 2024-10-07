c37bfb0fffdcd3725f2c847a47eefdabd0d79683
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-07 18:42:57 +0000

    chr

--- profile.js
+++ profile.js
@@ -36,8 +36,9 @@ document.addEventListener("DOMContentLoaded", function (){
             .then(status => {
                 // alert(status.info);
                 // Populate user details
-                document.getElementById("username").innerHTML = `<i class="fas fa-user"></i> ${status.username}`;
-                document.getElementById("email").innerHTML = `<i class="fas fa-envelope"></i> ${status.email}`;
+                document.getElementById("username").innerHTML = `<i class="fas fa-user"></i>  ${status.username}`;
+                document.getElementById("email").innerHTML = `<i class="fas fa-envelope"></i>  ${status.email}`;
+                document.getElementById("email").innerHTML = `<i class="fas fa-calendar-alt"></i> Since: ${status.date}`;
 
     
             })
