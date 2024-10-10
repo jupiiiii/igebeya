1c0ce581e296ab3abcf1f36fe71a9d5ea1d514ac
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-10 19:57:15 +0000

    jhsd

--- profile.js
+++ profile.js
@@ -106,6 +106,7 @@ document.addEventListener("DOMContentLoaded", function (){
         // Hash the password using SHA-256
         const hashedPassword = CryptoJS.SHA256(password).toString();
         alert("3");
+        
         const userDataDelete = {chatId: chatId, action: "delete", password: hashedPassword};
 
         alert("4");
