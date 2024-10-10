ad836ffab7f212421a15145c1905d2178fa8dfa3
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-10-10 19:34:27 +0000

    dd

--- profile.js
+++ profile.js
@@ -105,9 +105,10 @@ document.addEventListener("DOMContentLoaded", function (){
          
         // Hash the password using SHA-256
         const hashedPassword = CryptoJS.SHA256(password).toString();
+        alert("3");
         const userDataDelete = {chatId: chatId, action: "delete", password: hashedPassword};
 
-        alert("3");
+        alert("4");
         fetch(`https://igebeya3-272f297966dc.herokuapp.com/user_login`, {
             method: 'POST',
             headers: {
